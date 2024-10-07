8de9b234bdce99763b5affee4b841a255e841b19
Benjamin Clark <bclark@sudoblark.com>
2024-10-07 19:49:08 +0100

    Bump source in examples

--- examples/s3_bucket_notifications/main.tf
+++ examples/s3_bucket_notifications/main.tf
@@ -13,8 +13,7 @@ provider "aws" {
 }
 
 module "s3_bucket_notifications" {
-  #source = "github.com/sudoblark/sudoblark.terraform.module.aws.s3_bucket_notifications?ref=1.0.0"
-  source = "../../"
+  source = "github.com/sudoblark/sudoblark.terraform.module.aws.s3_bucket_notifications?ref=1.0.0"
 
   raw_notifications = local.raw_s3_bucket_notifications
 }
